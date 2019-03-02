5b923694c6cc2843df11490b8ea1d8c69c040430
kjgrover <kaje14@bellsouth.net>
2019-03-01 22:22:26 -0700

    added sentiment to echo

--- plumber.R
+++ plumber.R
@@ -1,11 +1,30 @@
 library(plumber)
+library(tidyverse)
+library(tidytext)
+library(syuzhet)
 
-#* Echo back the input
+#* Echo 
 #* @param msg The message to echo
 #* @get /echo
 function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+  # list(msg = paste0("The message is: '", msg, "'"))
+  
+afinnlib <- syuzhet:::afinn %>%
+  mutate(sentiment = ifelse(value < 0, "negative", "postive")) %>%
+  mutate(library = "afinn") #%>%
+
+msg <- tibble(line = 1:1, text = msg)
+
+output <- msg %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) %>%
+  merge(afinnlib, by = "word") %>%
+  mutate(score = value) %>%
+  select(score)
+
+
+output <- as.numeric(sum(output$score))
+
+return(output)
+
 }
-
-
-
